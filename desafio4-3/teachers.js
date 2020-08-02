489bc56d747982ce86e964c0ba7aaa75aa00cf96
rafaelfriederick <rafael.mf.documents@gmail.com>
2020-08-01 23:30:13 -0300

    Desafio 4-3

---
+++ desafio4-3/teachers.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+exports.post = function(req, res) {
+
+    const keys = Object.keys(req.body)
+    
+    for(key of keys) {
+        if(req.body[key] == "")
+        return res.send('Please, fill all fields!')
+    }
+    
+    let {avatar_url, name, birth, education_level, class_type, teacher_skill} = req.body
+    
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        education_level,
+        class_type,
+        teacher_skill,
+        created_at,
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!")
+    
+        return res.redirect("/teachers")
+    })
+}
